cd580b50969634ec96219ffbdcfa5be4a60249f1
pepsi <hainguyenn257@gmail.com>
2021-06-21 23:41:11 +0300

    Adding applitools eyes

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -6,7 +6,14 @@ const Bio = ({ headshot, name, tagline, role }) => {
   return (
     <div className={styles.bio}>
       <div className={styles.bioImage}>
-        <Image src={headshot} alt={`Avatar of ${name}`} />
+        {/* <Image
+          src={headshot}
+          alt={`Avatar of ${name}`}
+          width={1080}
+          height={1390}
+          layout="responsive"
+        /> */}
+        <img src={headshot} alt={`Avatar of ${name}`} />
       </div>
       <div className={styles.bioContent}>
         <p className={styles.bioContentName}>{name}</p>
